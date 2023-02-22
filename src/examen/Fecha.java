fc252a19cc285703b6beed36da57f49d3fb64caa
JaimeJimenez <dam05.2023@gmail.com>
2023-02-22 10:24:20 +0100

    Proyecto documentado

--- src/examen/Fecha.java
+++ src/examen/Fecha.java
@@ -1,7 +1,17 @@
 package examen;
 
 public class Fecha {
+	/**
+	 * @param mes
+	 * @param anio
+	 * @param dia
+	 * @return
+	 */
 	public static boolean validarFecha(int mes, int anio, int dia) {
+		/**
+		 * @author dam105
+		 * @version 1.0
+		 */
 		boolean fechaValida = extracted();
 		if ((dia > 0) && (mes > 0) && (anio > 0)) {
 			if ((mes <= 12) && (dia <= 31)) {
@@ -14,7 +24,7 @@ public class Fecha {
 				}
 			}
 		}
-		return fechaValida;
+		return fechaValida;	
 	}
 
 	public static boolean extracted() {
